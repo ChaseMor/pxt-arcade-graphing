16f98a58da1a5ab0a89d2e6b36d9b4b787595bbc
ChaseMor <C_Mortensen@live.cm>
2018-12-09 17:12:50 -0800

    Refactoring

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
-// tests go here; this will not be compiled when this package is used as a library
+namespace graphs {
+    let g: Graph = new Graph();
+    g.addXBasedFunction(function (x: number) {
+        return 5 * Math.sin(x);
+    }, 8);
+}
